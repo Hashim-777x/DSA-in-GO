79a1e6d139f5bac6789736ccc108674933a7ca95
hashim-777x <syedhashim721@gmail.com>
2026-06-16 23:54:27 +0530

    2sum two pointers

--- 01-arrays-hashing/2sum.go
+++ 01-arrays-hashing/2sum.go
@@ -35,3 +35,18 @@ func twoSumBruteForce(nums []int, target int) []int {
 	}
 	return nil
 }	
+
+func twoSumTwoPointers(nums []int, target int) []int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		sum := nums[left] + nums[right]
+		if sum == target {
+			return []int{left, right}
+		} else if sum < target {
+			left++
+		} else {
+			right--
+		}
+	}
+	return nil
+}
