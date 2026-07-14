6bd9b7d3bbe4f326ec2769f16e4b79ee6d274059
Hashim <113883060+Hashim-777x@users.noreply.github.com>
2026-07-15 03:13:02 +0530

    Time: 3 ms (40.19%) | Memory: 5.9 MB (53.40%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,15 +1,11 @@
 func twoSum(nums []int, target int) []int {
-      var  index = []int{0,0}
-    for i:=0 ; i<len(nums) ; i++{
-        for j:= i+1; j <len(nums);j++{
-          sum := nums[i]+nums[j]
-
-        if target == sum {
-         index[0]= i
-         index[1]= j
-            return index
-          }
+ seen := make(map[int]int)
+ for index, current := range nums {
+    x := target - current
+    if xIndex , value := seen[x] ; value {
+        return []int {xIndex, index}
     }
-    }
-        return index
-}
+    seen[current]= index
+ }
+ return []int{0,0}
+ }
