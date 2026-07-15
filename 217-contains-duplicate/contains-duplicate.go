92602557eb39090704292e86c08aaf533c548f8e
Hashim <113883060+Hashim-777x@users.noreply.github.com>
2026-07-16 01:25:09 +0530

    Time: 32 ms (24.54%) | Memory: 12 MB (42.01%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.go
+++ 217-contains-duplicate/contains-duplicate.go
@@ -1,9 +1,7 @@
 func containsDuplicate(nums []int) bool {
-    hashmap := make(map[int]bool, len(nums))
-    var numb int 
-    var val bool 
-    for _, numb = range nums  {
-        if _,val = hashmap[numb] ; val {
+    hashmap := make(map[int]bool, len(nums)) 
+    for _, numb := range nums  {
+        if _,val := hashmap[numb] ; val {
             return true 
         }
         hashmap[numb]= true
